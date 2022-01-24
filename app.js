e5445a73ec17f05303538745e84212f87f5e776a
Tsvetomir Ivanov <cvetomirkrasimirov96@abv.bg>
2022-01-24 14:11:30 +0200

    Add simple functionality and more styles to the game

---
+++ app.js
@@ -0,0 +1,37 @@
+const GAME_DUMMY_DATA = {
+  UNASSIGNED: 0,
+  TABLE_SIZE: 9,
+  TABLE_BOX: 3,
+  SUDOKU_NUMBERS: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  LEVELS: ["Easy", "Medium", "Hard"],
+  LEVEL_POINTS: [29, 38, 47],
+};
+
+const startGame = document.querySelector("#screen-start_game");
+
+let levels = 0;
+let level = GAME_DUMMY_DATA.LEVEL_POINTS[levels];
+
+document
+  .querySelector("#btn-difficulties")
+  .addEventListener("click", (event) => {
+    levels =
+      levels + 1 > GAME_DUMMY_DATA.LEVEL_POINTS.length - 1 ? 0 : levels + 1;
+    level = GAME_DUMMY_DATA.LEVEL_POINTS[levels];
+    event.target.innerHTML = GAME_DUMMY_DATA.LEVELS[levels];
+  });
+
+document.querySelector("#btn-new").addEventListener("click", () => {
+  alert("startgame");
+});
+
+const gameInfo = () => JSON.parse(localStorage.getItem("game"));
+
+const init = () => {
+  const game = gameInfo();
+  document.querySelector("#btn-continue").style.display = game
+    ? "grid"
+    : "none";
+};
+
+init();
